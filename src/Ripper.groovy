5f6ab2d36ecad6cb18f8a91a7b5377cb0be596e5
Wacław Borowiec <waclaw.borowiec@relayer.io>
2018-12-04 21:11:20 +0100

    initial commit

---
+++ src/Ripper.groovy
@@ -0,0 +1,45 @@
+
+
+COOKIE = '_ga=GA1.2.1759858195.1539513509; ASP.NET_SessionId=jencm33mtsb0vsssmbm34psz; _gid=GA1.2.237738438.1543868348; __RequestVerificationToken=nibKF3T9q_xbp5C4kh7zcRxRO75TW8knM7WyrbfBVoGa_AcA08ZghX0qx9Am0E0UEtoWNfRERsPSv8lPiKvLK7GZu1MVchERAFbgo_NTwII1; FedAuth=77u/PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz48U2VjdXJpdHlDb250ZXh0VG9rZW4gcDE6SWQ9Il84NDM0ZGYzZS0xMzM5LTQzMTAtODBkMy1lZWIyZDlmMjIwZGMtRThDQzNFNDY3NkFBMTA1RDJBMkNDMzE4QUUzMTI0RTEiIHhtbG5zOnAxPSJodHRwOi8vZG9jcy5vYXNpcy1vcGVuLm9yZy93c3MvMjAwNC8wMS9vYXNpcy0yMDA0MDEtd3NzLXdzc2VjdXJpdHktdXRpbGl0eS0xLjAueHNkIiB4bWxucz0iaHR0cDovL2RvY3Mub2FzaXMtb3Blbi5vcmcvd3Mtc3gvd3Mtc2VjdXJlY29udmVyc2F0aW9uLzIwMDUxMiI+PElkZW50aWZpZXI+dXJuOnV1aWQ6NWRkNjVjNjAtMmI3Yy00Zjg3LTg2ZjQtMTNmZTQxNzFiZDc0PC9JZGVudGlmaWVyPjxDb29raWUgeG1sbnM9Imh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwNi8wNS9zZWN1cml0eSI+QVFBQUFOQ01uZDhCRmRFUmpIb0F3RS9DbCtzQkFBQUFwQ1RzcHBaUFRrK0Z6TEZXZFJUN0h3QUFBQUFDQUFBQUFBQVFaZ0FBQUFFQUFDQUFBQUFTc1VFNVRKMmtHQndtc0JnV1IxYTJodzZ4RElCT0NQVWNDM1VqWVVSa25RQUFBQUFPZ0FBQUFBSUFBQ0FBQUFDMjZzbG9LSTlDcDRNcEwxcWNLQW42RDBQenJid1U4R0NidnM2ODQvcnZtcUFEQUFCMFdPQ011L25qTHZSanNQdDd0SU5xc2RMamcvNVdLUTRzTzVoUHRLeTRuZVl3eFFQdk5xVDAxUDRSRGkwRFlFSUZTeG02SjlXMXZRSDlhMjExSkcxSFBsSFF6bk1FZmJ4RWxyUEtqKy9aRGZuU3VuYmlXaFgrVXd5TkMzT3kwUkhxWEhLYkN1Y2NNM21MdG96RjluV0dCZWgwVzFUUFV1ZFIxU0JYOHV4ZC9rWDV5Uys4VmtZWVN0aWt0TkpXZHp6L0gwZlIzVjVUS3ZmZmlPU2M5aTllS2huUGtIaWRtTWtsb3NqVThxWHNsUDdMVkpKZ1lmU3cyL0gyKzZrS2h3a3lRMVJOOVhjN2RRd0o3ZDZPdU05M0hpN3Y1cjR6enRMWkcwazd0eGFOVFpZMHNjelc3UWtxL3FHWnhjTjl3M3FGL1IrMVZjNHdPTWtMb2ZMOVdHVWkxdmw5ZVg0czFlMWtwSk1oYkJUQjBmVkt0NDJkakJVV2dxNFZDaThnNWtFTlVmQkNOaWcvOStQbHRyRXVIUnhtQ056aWJjcWc0bXVkY3o0b3d1b2paMkdiSWNQU01sMHVNNUdNZG0xN2ROWk5jRlhnZnVJZTN3WG04d3hZRWoweXQvZzRIVUpuMnhkd3BINEtVL0FESWhsdjRGcmFmcTg3RjkyMGFwbEc4UXdQYXM2VVFtNTVkeE1ialkvWkdwTlI1V2E1M2NrRlJzSXRYZXZTQnBwY0FMWWMrdHh6di91Z3h4a1RjdmxVVTVRcW9vU0VwVTVObFVhZUsyanZKQW1sdkNyK0ZtbFZLVUs1U3p0OUovdTRqYlViZy9POW96UVZjbzFNTHFEcFc4amJobjJjQWRnSHhzQzJMK3p5WkdpL2U1RlQyVjZJbzFwNnhESml1RTV0SVpPUEUzMDlpSytIZDJjTHRGcEo4OWhUUW85U1ZDakNnVm56L1RFOFBSTU9OTXRFTCtvbWRFbE1aWDUrRFhXaFFJMnBEKytUSTJDQXFwMlZQOFpaWlNpWlVpMWVLT2FjeFUwNzVwUkFucm9ab2I1MGJNdEUvYnFJZWJnWWM1U2g3WFJDMVJ5SmozOFpSdlR3d3pxVGZVRUE1R2R2UVZRcmYrT1JyUTBoTEIzS2FnUlNZY0t3bGJa; FedAuth1=VXhUa09GY25LS0ZvT2pkVmFDR3Q3YW8zTTFyVlplM2lRNXlLcS9XSlUvcy9NMkYwdkMrcDNZbFFpcVZmSzhQU1F0V09QcDZQbWc1TW8rTVBxRzRYZTB6N053L09BNVExN1FoOFRDTXZ3QnRWZzFBZmltak5FNFp5a2hMMzlrS0U5N1lIYmRDUDFFUXlPMDEyQ0NDZ1YzLysrOXZEUFdlclRWVWxieENjVTUya0kwQjZRMG4yWHlYYWxvUlBXQy9wakdRR0o2TmVSUkNTblZpdXpKUXpMUUZ6MGxXZjF0bzF5dVc4Qlg2OHp6UXRJc1BTcWk4Tk1LZWlFMHFPU1lkTW1RM2pZSU8welgvTUFQNEk3Uk1oUHlUeHJBUGNqMll3aGVCVXhZUWxqaFd0OFFwb3RBWW9KMWZYV29lbDJxb1J6VkRGNE4rMVZBSHBrT096R0g1emp5ZXV4a2tnb05tS3Izalk1S1JuQmhqN3B2YTRNSnowQVFBQUFBRkx3azlPU2FLWmE2Z0MyekMrUEQ3bjUrRnJ1dTdSaWx1VUdCVzNOSXgvVVo4aTdZYlBtb2FCVXFSWVM3c0dnbUtSWUhYQ1NvQ2p1T0ZrNDU2YVRURUE9PC9Db29raWU+PC9TZWN1cml0eUNvbnRleHRUb2tlbj4=; eDeskAuth=JsUUd9m1fYEcB9LvINNmicBIf5rtdfGTf_qsiFdf4PUaMRvOtYoS4Cq2UZnS8ZNk_ZODXXltxckmECPLr9llx6EE0Mo20_2OAFcI36fJnoNfDzoSjEqRoRP0kYVrmPDEkciRhfgPKmYGR60he2xEj_7tFzBaiunnZenevP1cTqmE-WqIHcTcdsVS7SWnWirdg7kx72n57U2MbZlJUmu7P0_LTAButACelPWuWSLpreRDkxNvuNrf_EIMN4ezNPolrhGvIFEdmaY8bzZbFw6KshgMxXMuHXO6fgDiFiE8O5UgmvlHGRC7EvtEePwoUxpli1dliKDRdZm2RUh2g0i8Y-uecnegi3-6KVDOkqw6yLKsG2D08pB-M5fB5hV4AphCOgOrWFIR2ZujxyPNQF-uCDX-qx8ZF2sAJf7XsqAGezw_IlpwX2YyKfHrB7nkehLRe0yeWhOeaH9K-WetVVdi1egnmpZTPODE4io_3EzYT4P2JNG0xrUGe_73pI57SENebTIyWUduCV3XFbQZnDW_TQ; _gat=1'
+
+def rootUrl = "https://edesk.pearson.pl/Audio/"
+def mp3Url = rootUrl + "GetAudio?access_token="
+
+def download(def url, def consumer) {
+    def connection = new URL(url)
+            .openConnection() as HttpURLConnection
+
+    connection.setRequestProperty('Referer', 'https://edesk.pearson.pl/Audio/Index/1?p=-1')
+    connection.setRequestProperty('Cookie', COOKIE)
+
+// get the response code - automatically sends the request
+//println connection.responseCode + ": " + connection.inputStream.bytes
+    consumer(connection)
+
+}
+
+def getSaveFileConsumer(def file_name) {
+    return {
+        connection ->
+            def file = new File(file_name + '.mp3')
+            def bytes = connection.inputStream.bytes
+            println bytes
+            file.bytes = bytes
+    }
+}
+
+def xml = download(rootUrl + 'Index/1?p=1', {
+    connection ->
+        def text = connection.inputStream.text
+        println connection.responseCode + ": " + text
+        text
+    })
+
+def groups = (xml =~ /\"\/Audio\/GetAudio\?access_token=([0-9a-f\-]*)\"/)
+println groups.size()
+println 'res: ' + groups[0][1]
+def access_token = groups[0][1]
+
+
+download(mp3Url + access_token, getSaveFileConsumer('test'))
+//download(mp3Url + '496908cf-69d0-4438-bebb-960a6339dada', getSaveFileConsumer('1-2'))
